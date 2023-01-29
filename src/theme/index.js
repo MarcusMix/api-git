e77877804f322f2e4f6f9807fc587327b5a2fcca
Marcus Sandi <tradedomarcus@gmail.com>
2023-01-29 16:46:14 -0300

    feat: add theme of project

---
+++ src/theme/index.js
@@ -0,0 +1,18 @@
+export const defaultTheme = () => ({
+  colors: {
+    primary: "",
+    secondary: "",
+    lightGrey: "##90A19D",
+    blue: "#196774",
+    dark: "#363432",
+    cancel: "#DC030C",
+    sucess: "#6AA809"
+  },
+  background: {
+    primary: "#D7D9D8",
+    secondary: "#737065",
+    dark: "#212026",
+    cancel: "#DC030C",
+    sucess: "#6AA809"
+  },
+})
