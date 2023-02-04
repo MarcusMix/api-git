3cf8af46c32fa6c07aa5c4091d88b82d4520697e
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-04 14:59:46 -0300

    feat: add global styles to App

---
+++ src/theme/GlobalStyle.jsx
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: "Arimo", sans-serif;;
+    }
+
+    html, body {
+        animation: fadeIn 0.7s both;
+    }
+
+    @keyframes fadeIn {
+    0% {
+      opacity: 0;
+      transform: translateY(40px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0px);
+    }
+  }
+`
