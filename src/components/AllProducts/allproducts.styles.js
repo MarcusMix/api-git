df05e926a7403cf2df071f7f14bd895d979ded43
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-05 15:50:11 -0300

    style: card size and padding new measures

--- src/components/AllProducts/allproducts.styles.js
+++ src/components/AllProducts/allproducts.styles.js
@@ -6,8 +6,9 @@ export const ContainerCard = styled.div `
       display: flex;
       justify-content: center;
       align-items: center;
+      align-self: center;
       flex-wrap: wrap;
-      padding: 10px;
+      padding: 0 90px;
       align-content: center;
 `
 
@@ -15,22 +16,31 @@ export const ProductsCard = styled.div `
       background-color: ${(props) => props.theme.colors.primary};
       border-radius: 10px;
       box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
-      padding: 15px;
+      padding: 20px;
       text-align: center;
       margin: 20px auto;
       font-size: 1rem;
-      width: 300px;
-      height: 400px;
-      margin: 10px;
+      width: 280px;
+      height: 300px;
       transition: all 0.9s ease-out;
       :hover {
             cursor: pointer;    
       }
+
+      & p {
+            font-size: 20px;
+      }
 `
 
 export const ImageProduct = styled.img `
-      max-width: 50%;
-      margin: auto;
+      max-width: 100px;
+      max-height: 120px;
+      margin: 20px auto;
+
+      @media (max-width: 768px) {
+         max-width: 40%;
+      }
+      
 `
 
 export const ContainerProduct = styled(DivContainer) `
